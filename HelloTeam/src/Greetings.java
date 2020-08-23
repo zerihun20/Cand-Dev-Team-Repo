2d38340372884fdacb0f1594b3e5579afc6d590e
zerihun20 <zs20498@gmail.com>
2020-08-23 02:57:38 -0400

    This is my first java code

---
+++ HelloTeam/src/Greetings.java
@@ -0,0 +1,10 @@
+
+public class Greetings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello xxx fellow candidate developer of July 2020");
+
+	}
+
+}
